2da798905fce5bbf800c97ece4c155b97ba94dde
Yoori <noofy.fyda@icloud.com>
2026-09-26 19:50:37 +0800

    Add Google AdSense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import PayPalProvider from "@/components/PayPalProvider";
-
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,7 +40,13 @@ export default function RootLayout({
         <PayPalProvider>
           {children}
         </PayPalProvider>
-
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1682589334458752"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+ 
       </body>
 
     </html>
